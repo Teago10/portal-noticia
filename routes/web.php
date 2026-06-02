98ce01382c2a0b2c220b637097e5984a4555ce4e
thiago Affonso <thiagoaffonso452@gmail.com>
2026-06-02 20:23:38 -0300

    interface da home

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Controllers\SiteController;
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
-Route::get("/", [SiteController::class, "home"]);
+Route::get("/", [HomeController::class, "home"]);
