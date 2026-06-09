b41e5c992ee6ee9a8066d5d08d7393567f2d08fc
thiago Affonso <thiagoaffonso452@gmail.com>
2026-06-09 20:26:38 -0300

    criando a tabela categorias

---
+++ database/migrations/2026_06_09_231852_create_categorias_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Validation\Rules\Unique;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('categorias', function (Blueprint $table) {
+            $table->id();
+            $table->string('nome',50)->unique();
+            $table->string('descricao')->nullable();
+            $table->string('cor',7);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('categorias');
+    }
+};
