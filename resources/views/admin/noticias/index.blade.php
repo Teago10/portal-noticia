f373a442861ab0189842e4cadc0ac063c385fafe
thiago Affonso <thiagoaffonso452@gmail.com>
2026-06-17 19:42:54 -0300

    Tabela Noticia

---
+++ resources/views/admin/noticias/index.blade.php
@@ -0,0 +1,50 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+            Notícias
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900 dark:text-gray-100 flex justify-between items-center">
+                    <h1 class="text-lg bold ">Lista de Notícias</h1>
+                    <a href="#" class="bg-black text-white px-3 py-2 rounded">+ Nova Noticia</a>
+                </div>
+
+                <div class="text-white">
+
+                    <table>
+
+                        <thead>
+                            <tr>
+                                <th>ID</th>
+                                <th>Titulo</th>
+                                <th>Resumo</th>
+                                <th>Publicação</th>
+                                <th>Ação</th>
+                            </tr>
+                        </thead>
+
+                        <tbody>
+                            <tr>
+                                <td>1</td>
+                                <td>Titulo da noticia</td>
+                                <td>Resumo da noticia</td>
+                                <td>17/06/2026 19:40</td>
+                                <td>
+                                    <a href="#">Editar</a>
+                                    <a href="#">Excluir</a>
+                                </td>
+                            </tr>
+                        </tbody>
+
+                    </table>
+                    
+                </div>
+
+            </div>
+        </div>
+    </div>
+</x-app-layout>
--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -3,17 +3,21 @@
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div class="flex justify-between h-16">
             <div class="flex">
+
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
                     <a href="{{ route('dashboard') }}">
-                        <img src="img/logo-light.svg" alt="Portal Notícias">
+                        <img src="{{asset('img/logo-light.svg')}}" alt="Portal Notícias">   
                     </a>
                 </div>
 
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                        {{ __('Dashboard') }}
+                        Home
+                    </x-nav-link>
+                    <x-nav-link :href="route('admin.noticias.index')" :active="request()->routeIs('admin.noticias.index')">
+                        Noticias
                     </x-nav-link>
                 </div>
             </div>
