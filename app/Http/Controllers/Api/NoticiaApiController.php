6b4f0c3786105f59e55d435987170c27c46907af
thiago Affonso <thiagoaffonso452@gmail.com>
2026-08-12 20:48:14 -0300

    Api de notícias

--- app/Http/Controllers/Api/NoticiaApiController.php
+++ app/Http/Controllers/Api/NoticiaApiController.php
@@ -5,6 +5,10 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Models\Noticia;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Storage;
+
+use function Pest\Laravel\json;
 
 class NoticiaApiController extends Controller
 {
@@ -23,7 +27,34 @@ class NoticiaApiController extends Controller
      */
     public function store(Request $request) //POST
     {
-        //
+        $request->validate([
+            'titulo' => 'required',
+            'resumo' => 'required',
+            'conteudo' => 'required|min:20',
+            'categoria_id' => 'required',
+            'imagem' => 'nullable|image|mimes:jpeg,png,jpg,webp|max:2048'
+        ]);
+
+        $noticia = new Noticia();
+
+        $noticia->titulo = $request->titulo;
+        $noticia->resumo = $request->resumo;
+        $noticia->conteudo = $request->conteudo;
+        $noticia->categoria_id = $request->categoria_id;
+        $noticia->status = $request->status;
+        $noticia->usuario_id = Auth::user()->id;
+
+        if($request->hasFile('imagem')){
+            $noticia->imagem = $request->file('imagem')->store('noticia', 'public');
+        }
+
+        $noticia->save();
+
+
+        return response()->json([
+            'mensagem'=>'Notícia cadastrada com Sucesso',
+            'data'=>$noticia
+        ]);
     }
 
     /**
@@ -41,7 +72,39 @@ class NoticiaApiController extends Controller
      */
     public function update(Request $request, string $id) //PUT
     {
-        //
+        $request->validate([
+            'titulo' => 'required',
+            'resumo' => 'required',
+            'conteudo' => 'required|min:20',
+            'categoria_id' => 'required',
+            'imagem' => 'nullable|required|image|mimes:jpeg,png,jpg,webp|max:2048'
+        ]);
+
+        $noticia = Noticia::findOrFail($id);
+
+        $noticia->titulo = $request->titulo;
+        $noticia->resumo = $request->resumo;
+        $noticia->conteudo = $request->conteudo;
+        $noticia->categoria_id = $request->categoria_id;
+        $noticia->status = $request->status;
+        $noticia->usuario_id = Auth::user()->id;
+
+        if($request->hasFile('imagem')){
+
+            if($noticia->imagem){
+                Storage::disk('public')->delete($noticia->imagem);
+            }
+        
+            $noticia->imagem = $request->file('imagem')->store('noticia', 'public');
+        }
+
+        $noticia->save();
+
+
+        return response()->json([
+            'mensagem'=>'Notícia atualizada com Sucesso',
+            'data'=>$noticia
+        ]);
     }
 
     /**
@@ -49,6 +112,11 @@ class NoticiaApiController extends Controller
      */
     public function destroy(string $id) //DELETE
     {
-        //
+        $noticia = Noticia::findOrFail($id);
+        $noticia->delete();
+
+        return response()->json([
+            'mensagem' => 'Notícia removida com Sucesso'
+        ]);
     }
 }
